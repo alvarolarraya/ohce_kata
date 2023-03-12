e8c3cf721fdc6b100448029ecf04e85afc3d3553
Alvaro Larraya <alvaro.larraya1@gmail.com>
2023-03-12 20:12:03 +0000

    first stub added to test

--- src/Application/UserLoginService.php
+++ src/Application/UserLoginService.php
@@ -1,14 +1,44 @@
 <?php
 
 namespace UserLoginService\Application;
+use UserLoginService\Domain\User;
+use UserLoginService\Infrastructure\FacebookSessionManager;
+use Exception;
 
 class UserLoginService
 {
     private array $loggedUsers = [];
+    private FacebookSessionManager $apiCaller;
 
-    public function manualLogin(): string
+    public function __construct($apiCaller = FacebookSessionManager::class)
     {
-        return "user logged";
+        if(is_string($apiCaller)){
+            $this->apiCaller = new $apiCaller();
+        }else{
+            $this->apiCaller = $apiCaller;
+        }
     }
 
+    public function manualLogin(User $user): void
+    {
+        if(in_array($user,$this->loggedUsers,$strict=true)){
+            throw new Exception("User already logged in");
+        }
+        array_push($this->loggedUsers, $user);
+    }
+
+    public function getLoggedUsers(): array
+    {
+        return $this->loggedUsers;
+    }
+
+    public function addLoggedUserManuallyInList(User $user): void
+    {
+        array_push($this->loggedUsers, $user);
+    }
+
+    public function getExternalSessions(): int
+    {
+        return $this->apiCaller->getSessions();
+    }
 }
